5ef182bce17b008131af36d40af4ca0f5d6f9edf
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2024-11-01 16:41:21 +0300

    updated class names

--- src/test/java/io/github/mfaisalkhatri/ReadEmployeeDetailsAsArray.java
+++ src/test/java/io/github/mfaisalkhatri/ReadEmployeeDetailsAsArray.java
@@ -1,7 +1,5 @@
 package io.github.mfaisalkhatri;
 
-import static org.hamcrest.Matchers.equalTo;
-
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
@@ -9,7 +7,7 @@ import java.io.Reader;
 import com.google.gson.Gson;
 import io.github.mfaisalkhatri.pojo.EmployeeDetailsAsArray;
 
-public class ReadJsonArrayAsArray {
+public class ReadEmployeeDetailsAsArray {
 
     public static void main (String[] args) {
 
@@ -21,8 +19,9 @@ public class ReadJsonArrayAsArray {
             EmployeeDetailsAsArray employeeDetails = gson.fromJson (reader, EmployeeDetailsAsArray.class);
             System.out.println ("Printing all the Employee Details: " + employeeDetails);
             System.out.println (
-                "Printing the Employee Name from the first Json object: " + employeeDetails.getEmployees ().length);
-            System.out.println (employeeDetails.getEmployees ()[1].getName ());
+                "Printing the total number of JSON objects in the file: " + employeeDetails.getEmployees ().length);
+            System.out.println (
+                "Printing the Employee Name from the second Json object: " + employeeDetails.getEmployees ()[1].getName ());
 
         } catch (IOException e) {
             throw new Error ("Error reading JSON file");
---
+++ src/test/java/io/github/mfaisalkhatri/ReadEmployeeDetailsAsList.java
@@ -0,0 +1,42 @@
+package io.github.mfaisalkhatri;
+
+import static org.hamcrest.Matchers.equalTo;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import com.google.gson.Gson;
+
+import io.github.mfaisalkhatri.pojo.EmployeeDetails;
+import org.hamcrest.MatcherAssert;
+
+public class ReadEmployeeDetailsAsList {
+
+    public static void main (String[] args) {
+
+        Gson gson = new Gson ();
+        try (
+            Reader reader = new FileReader (
+                System.getProperty ("user.dir") + "/src/test/resources/employeedetails.json")) {
+
+            EmployeeDetails employeeDetails = gson.fromJson (reader, EmployeeDetails.class);
+            System.out.println ("Printing all the Employee Details: " + employeeDetails);
+            System.out.println (
+                "Printing the Employee Name from the first Json object: " + employeeDetails.getEmployees ()
+                    .get (0)
+                    .getName ());
+            System.out.println (
+                "Printing the Employee Designation from the second Json Object: " + employeeDetails.getEmployees ()
+                    .get (1)
+                    .getDesignation ());
+
+            MatcherAssert.assertThat (employeeDetails.getEmployees ()
+                .get (2)
+                .getEmail (), equalTo ("elizb@gmail.com"));
+
+        } catch (IOException e) {
+            throw new Error ("Error reading JSON file");
+        }
+
+    }
+}
